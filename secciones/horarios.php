8e0c1b38834be6689a6a8e723bdb9d45f5e61b11
Rafael Ramirez <rafael.ramirez@estudiante.uacm.edu.mx>
2023-04-10 17:33:01 -0500

    2:05:57

--- secciones/horarios.php
+++ secciones/horarios.php
@@ -1,3 +1,32 @@
 <?php
-//INSERT INTO `horarios` (`id`, `nombre`, `atencion`) VALUES (NULL, 'Desayuno', '10:00 - 12:00 h');
+include_once '../configuraciones/bd.php';
+$conexionBD=BD::crearInstancia();
+
+$id=isset($_POST['id'])?$_POST['id']:'';
+$nombre=isset($_POST['nombre'])?$_POST['nombre']:'';
+$atencion=isset($_POST['atencion'])?$_POST['atencion']:'';
+
+$alimetos=isset($_POST['alimentos'])?$_POST['alimentos']:'';
+$accion=(isset($_POST['accion']))?$_POST['accion']:'';
+
+if($accion!=""){
+    switch($accion){
+        case 'agregar':
+            $sql="INSERT INTO horarios (id, nombre, atencion) VALUES (NULL,:nombre, :atencion)";
+            $consulta=$conexionBD->prepare($sql);
+            $consulta->bindParam(':nombre',$nombre);
+            $consulta->bindParam(':atencion',$atencion);
+            $consulta->execute();
+            $idHorario=$conexionBD->lastInsertId();
+        break;
+    }
+}
+
+
+$sql = "SELECT * FROM horarios";
+$listaHorarios=$conexionBD->query($sql);
+$horarios=$listaHorarios->fetchAll();
+
+print_r($horarios);
+
 ?>
--- secciones/vista_horarios.php
+++ secciones/vista_horarios.php
@@ -1,5 +1,80 @@
 <?php  include('../templates/cabecera.php'); ?>
+<?php  include('../secciones/horarios.php'); ?>
 
-Vista horarios
+
+<div class="row">
+    <div class="col-5">
+        <form action="" method="post">
+            <div class="card">
+                <div class="card-header">
+                    Horarios 
+                </div>
+                
+                <div class="card-body">
+                    <div class="mb-3">
+                      <label for="id" class="form-label">ID</label>
+                      <input type="text"
+                        class="form-control" name="id" id="id" aria-describedby="helpId" placeholder="ID">
+                </div>
+
+                <div class="mb-3">
+                  <label for="nombre" class="form-label">Nombre</label>
+                  <input type="text"
+                    class="form-control" name="nombre" id="nombre" aria-describedby="helpId" placeholder="Escriba el nombre">
+                </div>
+
+                <div class="mb-3">
+                  <label for="atencion" class="form-label">Atencion</label>
+                  <input type="text"
+                    class="form-control" name="atencion" id="atencion" aria-describedby="helpId" placeholder="Escriba el horario de atención">
+                </div>
+
+                <div class="mb-3">
+                    <label for="" class="form-label">Alimentos:</label>
+                    <select multiple class="form control" name="alimentos[]" id="listaAlimentos">
+                        <option>Selecciona una opción</option>
+                        <option>Alimento 1</option>
+                        <option>Alimento 2</option>
+                    </select>
+                </div>
+
+                <div class="btn-group" role="group" aria-label="Button group name">
+                    <button type="submit" name="accion" value="agregar" class="btn btn-success">Agregar</button>
+                    <button type="submit" name="accion" value="editar" class="btn btn-primary">Editar</button>
+                    <button type="submit" name="accion" value="borrar" lass="btn btn-danger">Borrar</button>
+                </div>
+
+
+                </div>
+                <div class="card-footer text-muted">
+                </div>
+            </div>
+
+        </form>
+    </div>
+
+    <div class="col-7">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Nombre</th>
+                        <th>Acciones </th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <?php foreach($horarios as $horario): ?>
+                    <tr>
+                        <td><?php echo $horario['id'];?></td>
+                        <td><?php echo $horario['nombre'];?> <?php echo $horario['atencion'];?></td>
+                        <td>Seleccionar</td>
+                    </tr>
+                    <?php endforeach; ?>
+                </tbody>
+            </table>
+    </div>
+        
+
+</div>
 
 <?php include('../templates/pie.php'); ?>
