02a07cd9cbd5c14e89766db759e873c93760569b
Rafael Ramirez <rafael.ramirez@estudiante.uacm.edu.mx>
2023-04-14 22:36:12 -0500

    Funciona bien Tom Select

--- secciones/vista_horarios.php
+++ secciones/vista_horarios.php
@@ -86,4 +86,11 @@
         
 </div>
 
+<link href="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/css/tom-select.css" rel="stylesheet">
+<script src="https://cdn.jsdelivr.net/npm/tom-select@2.2.2/dist/js/tom-select.complete.min.js"></script>
+
+<script>
+    new TomSelect('#listaAlimentos');
+</script>
+
 <?php include('../templates/pie.php'); ?>
